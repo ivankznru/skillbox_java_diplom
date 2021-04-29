c0244f6f9e5cb29265b8ff10ac4f65515dd6b1fe
BALTALM\vkotov <tovkotov@yandex.ru>
2021-04-29 14:18:11 +0300

    ApiPostController code move to PostService

--- src/main/java/com/gh4biz/devpub/controllers/ApiPostController.java
+++ src/main/java/com/gh4biz/devpub/controllers/ApiPostController.java
@@ -2,215 +2,57 @@ package com.gh4biz.devpub.controllers;
 
 import com.gh4biz.devpub.model.entity.Post;
 import com.gh4biz.devpub.model.response.*;
-import com.gh4biz.devpub.repo.PostCommentsRepository;
-import com.gh4biz.devpub.repo.PostRepository;
-import com.gh4biz.devpub.repo.PostVotesRepository;
-import com.gh4biz.devpub.repo.Tag2PostRepository;
 import com.gh4biz.devpub.service.PostService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api")
 public class ApiPostController {
-    private PostRepository postRepository;
-    private PostVotesRepository postVotesRepository;
-    private PostCommentsRepository postCommentsRepository;
-    private Tag2PostRepository tag2PostRepository;
-    private final int ANNOUNCE_TEXT_LIMIT = 150;
-    private final int LIKE_VALUE = 1;
-    private final int DISLIKE_VALUE = -1;
-    private final int ACTIVE_POST_VALUE = 1;
+    private PostService postService;
 
-    public ApiPostController(PostRepository postRepository,
-                             PostVotesRepository postVotesRepository,
-                             PostCommentsRepository postCommentsRepository,
-                             Tag2PostRepository tag2PostRepository) {
-        this.postRepository = postRepository;
-        this.postVotesRepository = postVotesRepository;
-        this.postCommentsRepository = postCommentsRepository;
-        this.tag2PostRepository = tag2PostRepository;
+    public ApiPostController(PostService postService) {
+        this.postService = postService;
     }
 
     @GetMapping("/post")
-    private ResponseEntity<PostsResponse> postResponse(
-            @RequestParam int offset,
-            @RequestParam int limit,
-            @RequestParam String mode) {
-        if (mode.equals("popular")) {
-            return ResponseEntity.ok(popularPosts(offset, limit));
-        }
-        if (mode.equals("best")) {
-            return ResponseEntity.ok(bestPosts(offset, limit));
-        }
-        if (mode.equals("recent")) {
-            return ResponseEntity.ok(recentPosts(offset, limit));
-        }
-        if (mode.equals("early")) {
-            return ResponseEntity.ok(earlyPosts(offset, limit));
-        }
-        return ResponseEntity.noContent().build();
+    private ResponseEntity<PostsResponse> listOfPosts(
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "recent") String mode) {
+        return postService.getPosts(offset, limit, mode);
     }
 
     @GetMapping("/post/search")
     private ResponseEntity<PostsResponse> searchResponse(
-            @RequestParam int offset,
-            @RequestParam int limit,
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "10") int limit,
             @RequestParam String query
     ) {
-        return ResponseEntity.ok(getSearch(query, offset, limit));
+        return ResponseEntity.ok(postService.getSearch(query, offset, limit));
     }
 
     @GetMapping("/post/byDate")
-    private ResponseEntity<PostsResponse> postsResponseByDate(
-            @RequestParam int offset,
-            @RequestParam int limit,
+    private ResponseEntity<PostsResponse> getPostsByDate(
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "10") int limit,
             @RequestParam String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate localDate = LocalDate.parse(date, formatter);
-
-        int year = localDate.getYear();
-        int month = localDate.getMonthValue();
-        int day = localDate.getDayOfMonth();
-
-        ArrayList<Integer> posts =
-                postRepository.getPostsByDate(year, month, day,
-                        PageRequest.of(offset / limit, limit));
-
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        for (Integer postId : posts) {
-            Post post = postRepository.findPostsById(postId);
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        return ResponseEntity.ok(new PostsResponse(postAnnotationResponseList.size(), postAnnotationResponseList));
+        return ResponseEntity.ok(postService.getPostsByDate(offset, limit, date));
     }
 
     @GetMapping("/post/byTag")
-    private ResponseEntity<PostsResponse> postsResponseByTag(
-            @RequestParam int offset,
-            @RequestParam int limit,
+    private ResponseEntity<PostsResponse> getPostsByTag(
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "10") int limit,
             @RequestParam String tag) {
-
-        String tag2resp = "%".concat(tag).concat("%");
-        ArrayList<Integer> posts =
-                tag2PostRepository.getPosts(
-                        tag2resp,
-                        PageRequest.of(offset / limit, limit));
-
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        for (Integer postId : posts) {
-            Post post = postRepository.findPostsById(postId);
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        int count = tag2PostRepository.countTagPosts(tag2resp);
-        return ResponseEntity.ok(new PostsResponse(count, postAnnotationResponseList));
+        return ResponseEntity.ok(postService.getPostsByTag(offset, limit, tag));
     }
 
     @GetMapping("/post/{id}")
     private ResponseEntity<PostResponse> getPostById(
             @PathVariable int id) {
-        Post post = PostService.getPostRepository().findPostsById(id);
+        Post post = postService.getPostRepository().findPostsById(id);
         PostResponse postResponse = new PostResponse(post);
         return ResponseEntity.ok(postResponse);
-    }
-
-    private PostsResponse popularPosts(int offset, int limit) {
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-
-        Slice<CommentCount> postCommentSlice =
-                postCommentsRepository.countTotalComments(
-                        PageRequest.of(offset / limit, limit));
-
-        for (CommentCount comment : postCommentSlice) {
-            Post post = postRepository.findPostsById(comment.getId());
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-
-        return new PostsResponse(postCommentSlice.getContent().size(),
-                postAnnotationResponseList);
-    }
-
-    private PostsResponse bestPosts(int offset, int limit) {
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        Slice<VoteCount> voteCounts = postVotesRepository.countTotalVote(
-                LIKE_VALUE,
-                PageRequest.of(offset / limit, limit)
-        );
-        for (VoteCount vote : voteCounts) {
-            Post post = postRepository.findPostsById(vote.getId());
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        return new PostsResponse(voteCounts.getSize(), postAnnotationResponseList);
-    }
-
-    private PostsResponse recentPosts(int offset, int limit) {
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        Slice<Post> posts = postRepository.findAllByIsActiveOrderByTimeDesc(
-                ACTIVE_POST_VALUE,
-                PageRequest.of(offset / limit, limit)
-        );
-        for (Post post : posts) {
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        int count = postRepository.countAllByIsActive(ACTIVE_POST_VALUE);
-        return new PostsResponse(count, postAnnotationResponseList);
-    }
-
-    private PostsResponse earlyPosts(int offset, int limit) {
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        Slice<Post> posts = postRepository.findAllByIsActiveOrderByTimeAsc(
-                ACTIVE_POST_VALUE,
-                PageRequest.of(offset / limit, limit)
-        );
-        for (Post post : posts) {
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        return new PostsResponse(postRepository.countAllByIsActive(ACTIVE_POST_VALUE), postAnnotationResponseList);
-    }
-
-    private PostsResponse getSearch(String query, int offset, int limit) {
-        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
-        String query4sql = "%".concat(query).concat("%");
-        Page<Integer> posts = postRepository.getPostsBySearch(
-                query4sql,
-                PageRequest.of(offset / limit, limit)
-        );
-        for (Integer postId : posts) {
-            Post post = postRepository.findPostsById(postId);
-            postAnnotationResponseList.add(convert2Post4Response(post));
-        }
-        int count = postRepository.countSearchPosts(query4sql);
-        return new PostsResponse(count, postAnnotationResponseList);
-    }
-
-    private PostAnnotationResponse convert2Post4Response(Post post) {
-        return new PostAnnotationResponse(post.getId(),
-                post.getTime().getTime() / 1000,
-                new UserResponse(
-                        post.getUser().getId(),
-                        post.getUser().getName()),
-                post.getTitle(),
-                post.getText().substring // анонс
-                        (0, Math.min(ANNOUNCE_TEXT_LIMIT, post.getText().length()))
-                        .concat("..."),
-                getVoteCount(post, LIKE_VALUE),
-                getVoteCount(post, DISLIKE_VALUE),
-                getCommentsCount(post),
-                post.getViewCount());
-    }
-
-    private int getVoteCount(Post post, int vote) {
-        return postVotesRepository.countAllByPostIdAndValue(post.getId(), vote);
-    }
-
-    private int getCommentsCount(Post post) {
-        return postCommentsRepository.countAllByPostId(post.getId());
     }
 }
--- src/main/java/com/gh4biz/devpub/service/PostService.java
+++ src/main/java/com/gh4biz/devpub/service/PostService.java
@@ -1,26 +1,44 @@
 package com.gh4biz.devpub.service;
 
 import com.gh4biz.devpub.model.entity.Post;
+import com.gh4biz.devpub.model.response.*;
 import com.gh4biz.devpub.repo.PostCommentsRepository;
 import com.gh4biz.devpub.repo.PostRepository;
 import com.gh4biz.devpub.repo.PostVotesRepository;
+import com.gh4biz.devpub.repo.Tag2PostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 @Service
 public class PostService {
     private static PostRepository postRepository;
     private static PostCommentsRepository postCommentsRepository;
     private static PostVotesRepository postVotesRepository;
-    private static PostService instance;
+    private static Tag2PostRepository tag2PostRepository;
+
+    private final int ANNOUNCE_TEXT_LIMIT = 150;
+    private final int LIKE_VALUE = 1;
+    private final int DISLIKE_VALUE = -1;
+    private final int ACTIVE_POST_VALUE = 1;
+
     private PostService(PostRepository postRepository,
-                       PostCommentsRepository postCommentsRepository,
-                       PostVotesRepository postVotesRepository) {
+                        PostCommentsRepository postCommentsRepository,
+                        PostVotesRepository postVotesRepository,
+                        Tag2PostRepository tag2PostRepository) {
         this.postRepository = postRepository;
         this.postCommentsRepository = postCommentsRepository;
         this.postVotesRepository = postVotesRepository;
+        this.tag2PostRepository = tag2PostRepository;
     }
 
-    public static PostRepository getPostRepository() {
+    public PostRepository getPostRepository() {
         return postRepository;
     }
 
@@ -35,4 +53,147 @@ public class PostService {
     public static int getVoteCount(Post post, int vote) {
         return postVotesRepository.countAllByPostIdAndValue(post.getId(), vote);
     }
+
+    public ResponseEntity<PostsResponse> getPosts(int offset, int limit, String mode) {
+        if (mode.equals("popular")) {
+            return ResponseEntity.ok(popularPosts(offset, limit));
+        }
+        if (mode.equals("best")) {
+            return ResponseEntity.ok(bestPosts(offset, limit));
+        }
+        if (mode.equals("recent")) {
+            return ResponseEntity.ok(recentPosts(offset, limit));
+        }
+        if (mode.equals("early")) {
+            return ResponseEntity.ok(earlyPosts(offset, limit));
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    private PostsResponse popularPosts(int offset, int limit) {
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+
+        Slice<CommentCount> postCommentSlice =
+                postCommentsRepository
+                        .countTotalComments(
+                                PageRequest.of(offset / limit, limit));
+
+        for (CommentCount comment : postCommentSlice) {
+            Post post = postRepository.findPostsById(comment.getId());
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+
+        return new PostsResponse(postCommentSlice.getContent().size(),
+                postAnnotationResponseList);
+    }
+
+    private PostsResponse bestPosts(int offset, int limit) {
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        Slice<VoteCount> voteCounts = postVotesRepository.countTotalVote(
+                LIKE_VALUE,
+                PageRequest.of(offset / limit, limit)
+        );
+        for (VoteCount vote : voteCounts) {
+            Post post = postRepository.findPostsById(vote.getId());
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        return new PostsResponse(voteCounts.getSize(), postAnnotationResponseList);
+    }
+
+    private PostsResponse recentPosts(int offset, int limit) {
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        Slice<Post> posts = postRepository.findAllByIsActiveOrderByTimeDesc(
+                ACTIVE_POST_VALUE,
+                PageRequest.of(offset / limit, limit)
+        );
+        for (Post post : posts) {
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        int count = postRepository.countAllByIsActive(ACTIVE_POST_VALUE);
+        return new PostsResponse(count, postAnnotationResponseList);
+    }
+
+    private PostsResponse earlyPosts(int offset, int limit) {
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        Slice<Post> posts = postRepository.findAllByIsActiveOrderByTimeAsc(
+                ACTIVE_POST_VALUE,
+                PageRequest.of(offset / limit, limit)
+        );
+        for (Post post : posts) {
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        return new PostsResponse(postRepository.countAllByIsActive(ACTIVE_POST_VALUE), postAnnotationResponseList);
+    }
+
+    private PostAnnotationResponse convert2Post4Response(Post post) {
+        return new PostAnnotationResponse(post.getId(),
+                post.getTime().getTime() / 1000,
+                new UserResponse(
+                        post.getUser().getId(),
+                        post.getUser().getName()),
+                post.getTitle(),
+                post.getText().substring // анонс
+                        (0, Math.min(ANNOUNCE_TEXT_LIMIT, post.getText().length()))
+                        .concat("..."),
+                getVoteCount(post, LIKE_VALUE),
+                getVoteCount(post, DISLIKE_VALUE),
+                getCommentsCount(post),
+                post.getViewCount());
+    }
+
+    public PostsResponse getSearch(String query, int offset, int limit) {
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        String query4sql = "%".concat(query).concat("%");
+        Page<Integer> posts = postRepository.getPostsBySearch(
+                query4sql,
+                PageRequest.of(offset / limit, limit)
+        );
+        for (Integer postId : posts) {
+            Post post = postRepository.findPostsById(postId);
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        int count = postRepository.countSearchPosts(query4sql);
+        return new PostsResponse(count, postAnnotationResponseList);
+    }
+
+    private int getCommentsCount(Post post) {
+        return postCommentsRepository.countAllByPostId(post.getId());
+    }
+
+    public PostsResponse getPostsByDate(int offset, int limit, String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(date, formatter);
+
+        int year = localDate.getYear();
+        int month = localDate.getMonthValue();
+        int day = localDate.getDayOfMonth();
+
+        ArrayList<Integer> posts =
+                postRepository.getPostsByDate(year, month, day,
+                        PageRequest.of(offset / limit, limit));
+
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        for (Integer postId : posts) {
+            Post post = postRepository.findPostsById(postId);
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        return new PostsResponse(postAnnotationResponseList.size(), postAnnotationResponseList);
+    }
+
+    public PostsResponse getPostsByTag(int offset, int limit, String tag) {
+        String tag2resp = "%".concat(tag).concat("%");
+        ArrayList<Integer> posts =
+                tag2PostRepository.getPosts(
+                        tag2resp,
+                        PageRequest.of(offset / limit, limit));
+
+        ArrayList<PostAnnotationResponse> postAnnotationResponseList = new ArrayList<>();
+        for (Integer postId : posts) {
+            Post post = postRepository.findPostsById(postId);
+            postAnnotationResponseList.add(convert2Post4Response(post));
+        }
+        int count = tag2PostRepository.countTagPosts(tag2resp);
+        return new PostsResponse(count, postAnnotationResponseList);
+    }
+
 }
