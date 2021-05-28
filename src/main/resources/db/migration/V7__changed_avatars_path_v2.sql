ba5700495149e0087b72de53ea1f08f52133844c
BALTALM\vkotov <tovkotov@yandex.ru>
2021-05-28 05:22:38 +0300

    changed avatars path

---
+++ src/main/resources/db/migration/V7__changed_avatars_path_v2.sql
@@ -0,0 +1,14 @@
+UPDATE users
+SET photo = '/resources/static/img/54319.jpg' WHERE id = 1;
+
+UPDATE users
+SET photo = '/resources/static/img/54320.jpg' WHERE id = 2;
+
+UPDATE users
+SET photo = '/resources/static/img/54321.jpg' WHERE id = 3;
+
+UPDATE users
+SET photo = '/resources/static/img/54322.jpg' WHERE id = 4;
+
+UPDATE users
+SET photo = '/resources/static/img/54323.jpg' WHERE id = 5;
